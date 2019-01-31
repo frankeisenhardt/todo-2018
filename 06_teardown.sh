994f34e7a7ae9c25b29590c13f08fd901625e031
Rene Meyer <rene.meyer@de.ibm.com>
2019-01-31 15:37:25 +0100

    full teardown

--- 06_teardown.sh
+++ 06_teardown.sh
@@ -4,3 +4,4 @@ source ./env.local
 ibmcloud ks cluster-service-unbind --cluster $cluster_name --namespace $cluster_namespace --service $cloudant_svc_name
 ibmcloud resource service-instance-delete $cloudant_svc_name
 kubectl delete -f deploy2kube.yaml
+s3cmd del --force --recursive -v --host-bucket=$host_bucket --host=$host_base --access_key=$access_key_id --secret_key=$secret_access_key -v --acl-private s3://$bucket_name
